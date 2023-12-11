f5c8db3a3d26587c2c1143640f59564cacc77865
AlberteVallentin <alberte@vallentin.eu>
2023-12-11 12:56:07 +0100

    Start

---
+++ src/main/java/com/example/quiz/QuizController.java
@@ -0,0 +1,34 @@
+package com.example.quiz;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class QuizController {
+
+    @FXML
+    public Label question;
+    
+    @FXML
+    public Button optionOne, optionTwo, optionThree, OptionFour;
+
+    @FXML
+    private void initialize(){
+        
+    }
+
+    @FXML 
+    private void optionOneClicked(ActionEvent event){
+        
+    }
+
+    public void optionTwoClicked(ActionEvent event) {
+    }
+
+    public void optionThreeClicked(ActionEvent event) {
+    }
+
+    public void optionFourClicked(ActionEvent event) {
+    }
+}
